a0b504044b812db08140a973d4ca7cface4a06f9
Matheus Ferreira <matheusfeereirabispo1380@gmail.com>
2025-02-21 10:35:15 -0300

    parametrizando os filtros

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components'
 
 import { Props } from '.'
 
+type propsSemLegendaEContador = Omit<Props, 'contador' | 'legenda'>
+
 //estilo do card
 
-export const Card = styled.div<Props>`
+export const Card = styled.div<propsSemLegendaEContador>`
   padding: 8px;
   border: 1px solid ${(props) => (props.ativo ? '#1e90ff' : '#a1a1a1')};
   background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
